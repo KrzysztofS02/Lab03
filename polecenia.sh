88afe6dd16efe34ea20fa532925ecfc2b3aac47a
Krzysztof <dsw51744@student.dsw.edu.pl>
2024-03-20 08:37:42 +0100

    First commit

---
+++ polecenia.sh
@@ -0,0 +1,42 @@
+show commands - wszystkie komemdy
+git commit
+git branch 
+git checkout 
+git switch
+git checkout -b
+git merge
+git rebase
+git log
+git checkout <nazwa-gałęzi>^
+git checkout <nazwa-gałęzi>^^
+git checkout <nazwa-gałęzi>~^2^3
+git checkout HEAD~4
+git branch -f <nazwa-gałęzi> <oznaczenie-commita>
+git branch -f <nazwa-gałęzi> HEAD~3
+git reset HEAD~1
+git revert
+git cherry-pick <Commit1> <Commit2> <...>
+git rebase -i HEAD~4
+git rebase -i <nazwa-gałęzi>
+undo
+reset
+show solution
+git commit --amend
+git describe
+git bisect
+git clone
+git fetch
+git pull
+git fakeTeamwork
+git fakeTeamwork foo 3
+git push
+git pull
+git checkout -b feature C2
+git reset o/main
+git push origin feature
+git checkout -b <nazwa-gałęzi> o/main
+git branch -u o/main <nazwa-gałęzi>
+git push main <nazawa-gałezi>
+git push origin foo^:main
+git fetch origin foo~1:bar
+git pull origin main:side
